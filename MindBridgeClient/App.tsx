b3234a810df5d1c786106245daab9223a8d61e66
chlorUpload <chlor.upload@gmail.com>
2020-10-11 21:53:20 +0900

    GoogleSignIn 관련 업데이트

--- MindBridgeClient/App.tsx
+++ MindBridgeClient/App.tsx
@@ -8,7 +8,7 @@ import CombineReducer from './src/CombineReducer';
 
 const theme = {
   ...DefaultTheme,
-  colors: {  
+  colors: {
     ...DefaultTheme.colors,
     primary: '#F970B9',
     surface: 'white',
@@ -16,74 +16,76 @@ const theme = {
   }
 }
 
-export default function App() {
-  return (
-    <ReduxProvider store={createStore(CombineReducer, {
-      Count: {
-        Count: 0,
-      },
-      Card: {
-        OverviewSurveyCategory: {
-          Title: "테스트",
-          Cards: [],
+export default class App extends React.Component {
+  render() {
+    return (
+      <ReduxProvider store={createStore(CombineReducer, {
+        Count: {
+          Count: 0,
         },
-        OverviewVideoCategory: {
-          Title: "추천 영상",
-          Cards: [],
-        },
-        OverviewColumnCategory: {
-          Title: "추천 칼럼",
-          Cards: [],
-        },
-        SurveyCategories: [
-          {
-            Title: "테스트1",
+        Card: {
+          OverviewSurveyCategory: {
+            Title: "테스트",
             Cards: [],
           },
-          {
-            Title: "테스트2",
+          OverviewVideoCategory: {
+            Title: "추천 영상",
             Cards: [],
           },
-        ],
-        TipVideoCategory: {
-          Title: "실전 팁 영상",
-          Cards: [],
+          OverviewColumnCategory: {
+            Title: "추천 칼럼",
+            Cards: [],
+          },
+          SurveyCategories: [
+            {
+              Title: "테스트1",
+              Cards: [],
+            },
+            {
+              Title: "테스트2",
+              Cards: [],
+            },
+          ],
+          TipVideoCategory: {
+            Title: "실전 팁 영상",
+            Cards: [],
+          },
+
+          TipCategory: {
+            Title: "실전 팁 칼럼",
+            Cards: [],
+          },
         },
-    
-        TipCategory: {
-          Title: "실전 팁 칼럼",
-          Cards: [],
+        Login: {
+          autoLogin: true,
+          loggedIn: false,
+          needRegister: false,
+          user: null,
+          idToken: null,
         },
-      },
-      Login: {
-        autoLogin: true,
-        loggedIn: false,
-        needRegister: false,
-        user: null,
-        idToken: null,
-      },
-      Privacy: {
-        UserInfo: {
-          name: "",
-          birthDay: new Date(),
-          image: "",
-          sex: "female",
+        Privacy: {
+          UserInfo: {
+            name: "",
+            birthDay: new Date(),
+            image: "",
+            sex: "female",
+          },
+          SpouseInfo: {
+            name: "",
+            birthDay: new Date(),
+            image: "",
+            sex: "male",
+          }
         },
-        SpouseInfo: {
-          name: "",
-          birthDay: new Date(),
-          image: "",
-          sex: "male",
+        Survey: {
+          SurveyResultCards: [],
+          SpouseResultCards: [],
         }
-      },
-      Survey: {
-        SurveyResultCards: [],
-        SpouseResultCards: [],
-      }
-    } ,applyMiddleware(ReduxThunk))}>
-      <PaperProvider theme={theme}>
-        <Main />
-      </PaperProvider>
-    </ReduxProvider>
-  );
+      }, applyMiddleware(ReduxThunk))}>
+        <PaperProvider theme={theme}>
+          <Main />
+        </PaperProvider>
+      </ReduxProvider>
+    );
+  }
 }
