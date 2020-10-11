10b7bd01da8f2e5c8295b0c3f2ae6fab42e3bcb3
chlorUpload <chlor.upload@gmail.com>
2020-10-11 21:53:41 +0900

    함수형에서 클래스형으로 변경

--- MindBridgeClient/src/Main.tsx
+++ MindBridgeClient/src/Main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { createStackNavigator, HeaderTitle, StackHeaderTitleProps } from '@react-navigation/stack';
 import { NavigationContainer } from "@react-navigation/native";
 
-import LoginScreen        from "./screens/LoginScreen";
-import MainScreen         from "./screens/MainScreen";
-import TestScreen         from "./screens/TestScreen";
-import SurveyWebScreen    from "./screens/SurveyWebScreen";
+import LoginScreen from "./screens/LoginScreen";
+import MainScreen from "./screens/MainScreen";
+import TestScreen from "./screens/TestScreen";
+import SurveyWebScreen from "./screens/SurveyWebScreen";
 import SurveyResultScreen from "./screens/SurveyResultScreen";
 import LogoTitle from "./components/LogoTitle";
 import SurveyHistoryScreen from "./screens/SurveyHistoryScreen";
@@ -22,96 +22,98 @@ import SpouseHistoryScreen from "./screens/SpouseHistoryScreen";
 
 const Stack = createStackNavigator();
 
-export default function Main() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerStyle: {
-          backgroundColor: '#FCDCFA'
-        },
-      }}>
-        <Stack.Screen name="Login"        component={LoginScreen} 
-                      options={{
-                        headerShown: false,
-                      }}
-        />
-        <Stack.Screen name="Register"     component={RegisterScreen}
-                      options={{
-                        headerTitle: "회원가입"
-                      }}
-        />
-        <Stack.Screen name="Main"         component={MainScreen}
-                      options={{
-                        headerShown: false,
-                      }}
-        />
-        <Stack.Screen name="Test"         component={TestScreen} />
-        <Stack.Screen name="SurveyWeb"    component={SurveyWebScreen} 
-                      options={{
-                        headerTitle: "테스트"
-                      }}
-        />
-        <Stack.Screen name="VideoWeb"    component={VideoWebScreen} 
-                      options={{
-                        headerShown: false,
-                      }}
-        />
-        <Stack.Screen name="TipWeb"    component={TipWebScreen} 
-                      options={{
-                        headerTitle: "실전 부부 팁"
-                      }}
-        />
-        <Stack.Screen name="SurveyResult" component={SurveyResultScreen} 
-                      options={{
-                        headerTitle: "테스트 결과"
-                      }}
-        />
-        <Stack.Screen name="SurveyHistory" component={SurveyHistoryScreen}
-                      options={{
-                        headerTitle: "테스트 기록"
-                      }}
-        />
-        <Stack.Screen name="SpouseHistory" component={SpouseHistoryScreen}
-                      options={{
-                        headerTitle: "배우자의 테스트 기록"
-                      }}
-        />
+export default class Main extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {
+            backgroundColor: '#FCDCFA'
+          },
+        }}>
+          <Stack.Screen name="Login" component={LoginScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="Register" component={RegisterScreen}
+            options={{
+              headerTitle: "회원가입"
+            }}
+          />
+          <Stack.Screen name="Main" component={MainScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="Test" component={TestScreen} />
+          <Stack.Screen name="SurveyWeb" component={SurveyWebScreen}
+            options={{
+              headerTitle: "테스트"
+            }}
+          />
+          <Stack.Screen name="VideoWeb" component={VideoWebScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="TipWeb" component={TipWebScreen}
+            options={{
+              headerTitle: "실전 부부 팁"
+            }}
+          />
+          <Stack.Screen name="SurveyResult" component={SurveyResultScreen}
+            options={{
+              headerTitle: "테스트 결과"
+            }}
+          />
+          <Stack.Screen name="SurveyHistory" component={SurveyHistoryScreen}
+            options={{
+              headerTitle: "테스트 기록"
+            }}
+          />
+          <Stack.Screen name="SpouseHistory" component={SpouseHistoryScreen}
+            options={{
+              headerTitle: "배우자의 테스트 기록"
+            }}
+          />
 
-        {/* subscreens */}
-        <Stack.Screen name="Announce" component={AnnounceScreen}
-                      options={{
-                        headerTitle: "공지사항"
-                      }}
-        />
-        <Stack.Screen name="AppInfo" component={AppInfoScreen}
-                      options={{
-                        headerTitle: "앱 정보"
-                      }}
-        />
-        <Stack.Screen name="Ask" component={AskScreen}
-                      options={{
-                        headerTitle: "문의하기"
-                      }}
-        />
-        <Stack.Screen name="FAQ" component={FAQScreen}
-                      options={{
-                        headerTitle: "자주 묻는 질문"
-                      }}
-        />
-        <Stack.Screen name="Point"    component={PointScreen} 
-                      options={{
-                        headerTitle: "My 포인트"
-                      }}
-        />
-        <Stack.Screen name="Reference"    component={ReferenceScreen} 
-                      options={{
-                        headerTitle: "자료실"
-                      }}
-        />
+          {/* subscreens */}
+          <Stack.Screen name="Announce" component={AnnounceScreen}
+            options={{
+              headerTitle: "공지사항"
+            }}
+          />
+          <Stack.Screen name="AppInfo" component={AppInfoScreen}
+            options={{
+              headerTitle: "앱 정보"
+            }}
+          />
+          <Stack.Screen name="Ask" component={AskScreen}
+            options={{
+              headerTitle: "문의하기"
+            }}
+          />
+          <Stack.Screen name="FAQ" component={FAQScreen}
+            options={{
+              headerTitle: "자주 묻는 질문"
+            }}
+          />
+          <Stack.Screen name="Point" component={PointScreen}
+            options={{
+              headerTitle: "My 포인트"
+            }}
+          />
+          <Stack.Screen name="Reference" component={ReferenceScreen}
+            options={{
+              headerTitle: "자료실"
+            }}
+          />
 
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 
